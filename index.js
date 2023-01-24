fd0440087f8bb78e683c100ea45358410074df42
nikhil063 <122106278+nikhil063@users.noreply.github.com>
2023-01-24 19:12:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const timeEL = document.querySelector('.watch .time');
-const startButton = document.getElementById("start");
-const stopButton = document.getElementById("stop");
-const resetButton = document.getElementById("reset");
+const timeEL = document.getElementById("time");
 
 let TotalSeconds = 0;
 let interval = null;
@@ -13,31 +10,29 @@ function timer () {
 	let minutes = Math.floor((TotalSeconds - (hours * 3600)) / 60);
 	let seconds = TotalSeconds % 60;
 
-	if (seconds < 10) seconds = "0" + seconds;
-	if (minutes < 10) minutes = "0" + minutes;
-	if (hours < 10) hours = "0" + hours;
+	seconds = seconds < 10 ?  "0" + seconds: seconds;
+	minutes = minutes < 10 ?  "0" + minutes: minutes;
+	hours = hours < 10 ? "0" + hours: hours;
 
 	timeEL.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-function Fstart () {
+function start () {
 	if (interval) {
 		return
 	}
-	interval = setInterval(timer, 1000);
+	else {
+		interval = setInterval(timer, 1000);
+	}
 }
 
-function Fstop () {
+function stop () {
 	clearInterval(interval);
 	interval = null;
 }
 
-function Freset () {
-	Fstop();
+function reset () {
+	stop();
 	TotalSeconds = 0;
 	timeEL.innerText = '00:00:00';
 }
-
-startButton.addEventListener("click", Fstart);
-stopButton.addEventListener("click", Fstop);
-resetButton.addEventListener("click", Freset);
